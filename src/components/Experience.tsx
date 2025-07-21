68f10e324ffa08e24c6994d7529c8a1e0ea766ce
jkgithub02 <jkwork02@gmail.com>
2025-07-22 00:39:59 +0800

    experience sections with list for job descriptions

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -10,7 +10,7 @@ const Experience = () => {
         {/* TODO: Update Experience structure */}
         {
           experiences.map((exp) => (
-            <article key={exp.position} className='bg-white dark:bg-black/30 rounded-lg shadow-md p-6'>
+            <article key={exp.position} className='bg-white dark:bg-black/30 rounded-lg shadow-md p-10 mb-5'>
               <h2 className='text-xl font-extrabold mb-2'>
                 {exp.position} <span className='text-sm font-semibold'>{exp.start} - {exp.end} {exp.year}</span>
               </h2>
@@ -18,8 +18,10 @@ const Experience = () => {
               <h3 className='text-2xl font-semibold mb-2'>
                 {exp.company}
               </h3>
-              <ul className="text-gray-600 dark:text-gray-300 mb-4">
-                {exp.description}
+              <ul className="text-gray-600 dark:text-gray-300 text-lg mb-4 list-disc list-inside space-y-1">
+                {exp.description.map((item, index) =>(
+                  <li key={index}>{item}</li>
+                ))}
               </ul>
 
               
