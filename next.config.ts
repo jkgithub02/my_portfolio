5411d7297168fee682fdc621db7e0c77e2dc900c
jkgithub02 <jkwork02@gmail.com>
2025-07-22 14:34:49 +0800

    image path fix#2

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   basePath: '/my_portfolio',
-  assetPrefix: '/my_portfolio/',
   images: {
     unoptimized: true,
   },
